8a50c0f40f681214b32918d84cd10d2d3aa1a5fe
Nguyễn Tiến Trung <mr.trung0104@gmail.com>
2020-06-24 11:00:28 +0700

    update

---
+++ ClientSide/src/main/java/com/example/clientside/controller/ControllerMapping.java
@@ -0,0 +1,50 @@
+package com.example.clientside.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ControllerMapping {
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @RequestMapping("/register")
+    public String register() {
+        return "register";
+    }
+    @RequestMapping("/home")
+    public String home(){
+        return "index";
+    }
+    @RequestMapping("/cart")
+    public String cart(){
+        return "cart";
+    }
+    @RequestMapping("/favourite")
+    public String favourite(){
+        return "favourite";
+    }
+    @RequestMapping("/confirmation")
+    public String confirmation(){
+        return "confirmation";
+    }
+    @RequestMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+    @RequestMapping("/product-details")
+    public String productDetails(@RequestParam("id") String id){
+        return "single-details";
+    }
+    @RequestMapping("/shop")
+    public String shop(){
+        return "category";
+    }
+    @RequestMapping("/403")
+    public String notPo(){
+        return "403";
+    }
+
+}
