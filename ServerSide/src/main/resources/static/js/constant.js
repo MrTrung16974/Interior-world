d8d510c5bab22ee0022c6cb235eece33481b80e3
Nguyễn Tiến Trung <mr.trung0104@gmail.com>
2020-07-15 12:07:38 +0700

    update

--- ServerSide/src/main/resources/static/js/constant.js
+++ ServerSide/src/main/resources/static/js/constant.js
@@ -1,5 +1,11 @@
 var pathname = window.location.pathname;
 
+// formart price
+var formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
+
 //    switch option
 switch (pathname) {
     case "/home":
@@ -30,40 +36,3 @@ switch (pathname) {
         $("title.title-page").text("Aroma Admin");
         break;
 }
-
-/**
- * Chuc nang tim kiem, tai lai bang du lieu dua theo thong tin loc
- * */
-// jQuery(document).ready(function () {
-//     $('#toDate').datepicker({
-//         format: "dd/mm/yyyy",
-//         language: "vi",
-//         //            endDate: "0d",
-//         todayHighlight: true,
-//         autoclose: true,
-//
-//     });
-//     $('#iaddFromDate').on('click', function () {
-//         $('#fromDate').focus();
-//     });
-//     $('#iaddToDate').on('click', function () {
-//         $('#toDate').focus();
-//     });
-//     $('#fromDate').datepicker({
-//         format: "dd/mm/yyyy",
-//         language: "vi",
-//         //            endDate: "0d",
-//         todayHighlight: true,
-//         autoclose: true
-//     });
-//     var toDate = new Date();
-//     fromDate = new Date();
-//     fromDate.setMonth(fromDate.getMonth() - 1);
-//     toDate.setHours(0);
-//     toDate.setMinutes(0);
-//     toDate.setSeconds(0);
-//     toDate.setMilliseconds(0);
-//     $('#fromDate').datepicker('setDate', fromDate);
-//     $('#toDate').datepicker('setDate', toDate);
-//
-// });
