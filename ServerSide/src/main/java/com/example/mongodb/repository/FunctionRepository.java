3b7252eb8b2cac49a4dbc2c5d342eb6852e159dc
Nguyễn Tiến Trung <mr.trung0104@gmail.com>
2020-07-17 20:08:37 +0700

    update

--- ServerSide/src/main/java/com/example/mongodb/repository/FunctionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mongodb.repository;
-
-import com.example.mongodb.model.Function;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FunctionRepository extends MongoRepository<Function,String> {
-}
